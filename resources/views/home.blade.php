d548227caf64130a608e81f02baf0e1c4d7e0709
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-25 17:13:25 +0200

    add home view with posts loop and external stylesheet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Blog</title>
-    <style>
-      body { font-family: sans-serif; max-width: 800px; margin: 2rem auto; padding: 0 1rem; }
-    </style>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  <h1>My Blog</h1>
-  <p>Welcome!</p>
+    <h1>My Blog</h1>
+  
+    @foreach ($posts as $post)
+        <article>
+            <h2>{{ $post['title'] }} </h2>
+            <p>{{ $post['body'] }}</p>
+        </article>
+    @endforeach
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $posts = [
+        ['title' => 'My First Post', 'body' => 'This is my first post!'],
+        ['title' => 'Learning Laravel', 'body' => 'Laravel is quite similar to Express'],
+        ['title' => 'Running on a Pi', 'body' => 'Served from a Pi W Zero'],
+    ];
+
+    return view('home', ['posts' => $posts]);
 });
